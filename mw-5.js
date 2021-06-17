3abf796a877b254c0126338a0ed9bb0b59b71953
woodlik <sqvoch@gmail.com>
2021-06-17 16:40:44 +0300

    p

--- mw-5.js
+++ mw-5.js
@@ -1,1 +1,15 @@
-console.log(1111)
+let urlParams = window.location.href;
+urlParams = urlParams.split("?")[1];
+
+let cdnOriginalUrl = "https://apo-api.mws.test/" + urlParams + "",
+  altParagraph = document.createElement("p");
+altParagraph.style.textAlign = "center";
+let cdnOriginalLink = document.createElement("a");
+(cdnOriginalLink.style.margin = "20px"),
+  (cdnOriginalLink.href = cdnOriginalUrl + ""),
+  cdnOriginalLink.appendChild(
+    document.createTextNode("Download original file (alternative link)")
+  );
+
+altParagraph.appendChild(cdnOriginalLink),
+  document.body.appendChild(altParagraph);
